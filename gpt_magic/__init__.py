a14e024b3a3b1ade43f5151c87b77c33908b23b7
butchi <butchiyu+com-github@gmail.com>
2025-11-16 20:09:47 +0900

    絵文字は使わない

--- gpt_magic/__init__.py
+++ gpt_magic/__init__.py
@@ -3,7 +3,7 @@ from .core import register_gpt_magic
 def load_ipython_extension(ipython):
     register_gpt_magic()
     print(
-        "✅ GPTマジック（%%gpt）を登録しました。"
+        "GPTマジック（%%gpt）を登録しました。"
         "セルの先頭に %%gpt 、続いてメッセージを入れてShift + Enterを押してください。"
         "また、最初にAPIキーをシークレットの openaiApiKey にセットしてください。"
     )
